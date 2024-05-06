6bb660d5fdc3a75758fd215bd7882d22be802a92
BCCanDataAnalytics <83374981+BCCanDataAnalytics@users.noreply.github.com>
2024-05-06 15:17:02 -0700

    add sex colors

--- color_palette.R
+++ color_palette.R
@@ -1,6 +1,13 @@
+## 26 categorical colors
+## Can be used for coloring cancer types and age groups
 myColors <- c( "#a6228c", "#02bbd3", "#97989a", "#fcaf33","#7AD151FF", "#004f59",
                "#dbdb8d", "#ff9896", "#1f77b4", "#c49c94", "#7f7f7f",
                "#c7c7c7", "#d62728", "#e377c2", "#f7b6d2", "#c5b0d5",
                "#98df8a", "#9edae5", "#ffbb78", "#9467bd", "#aec7e8",
                "#000000", "#bcbd22", "#8c564b", "#2ca02c", "#ff7f0e")
+
+
+## sex colors
+sexColor <- setNames(c("#a6228c", "#02bbd3"), c("Female", "Male"))
+
 
