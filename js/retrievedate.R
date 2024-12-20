e0b27bdbae3903e2981ff8390862de1cc7ebc5fe
Rhian Stephens <124310949+rhistephens@users.noreply.github.com>
2024-12-20 10:14:48 -0800

    Update retrieve date for 2022 live apps

--- js/retrievedate.R
+++ js/retrievedate.R
@@ -5,9 +5,9 @@ p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "dat
 
 # For the apps with data updating every two years, refer to the following code
 p("Data Source: BC Cancer Registry")
-p("Date Retrieved:  08 Mar 2022")
+p("Date Retrieved:  07 Sep 2023")
 p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
 
 # For 2021 data
-p("Date Retrieved:  07 Sep 2023")
+p("Date Retrieved:  16 Aug 2024")
