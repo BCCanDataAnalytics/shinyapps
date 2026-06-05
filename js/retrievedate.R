f8493d6cbdf7239a00fe0bab194939b5603e24ab
Alpamys-I <alpamys.issanov1@bccancer.bc.ca>
2026-06-05 09:15:37 -0700

    Update retrievedate.R
    Changed the team name

--- js/retrievedate.R
+++ js/retrievedate.R
@@ -1,12 +1,12 @@
 p("Data Source: BC Cancer Registry")
 p("Date Retrieved:  16 Aug 2024")
-p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
+p("Analysis by: Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
 
 # For the apps with data updating every two years, refer to the following code
 p("Data Source: BC Cancer Registry")
 p("Date Retrieved:  07 Sep 2023")
-p("Analysis by: Cancer Surveillance and Outcomes, Data and Analytics, BC Cancer")
+p("Analysis by: Data and Analytics, BC Cancer")
 p("Contact:", tags$a(href="mailto:datareq@bccancer.bc.ca", target="_blank", "datareq@bccancer.bc.ca"))
 
 # For 2023 data
